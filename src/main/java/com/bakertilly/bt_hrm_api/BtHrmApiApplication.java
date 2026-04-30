c552f0f59fb5e5140b6e572fd0bdeba593607d0c
pissanu <pissanu@bakertilly.co.th>
2026-04-30 16:43:12 +0700

    initial project

---
+++ src/main/java/com/bakertilly/bt_hrm_api/BtHrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.bakertilly.bt_hrm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtHrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtHrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakertilly/bt_hrm_api/BtHrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakertilly.bt_hrm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtHrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
